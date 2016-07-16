af6c0126ce6b7def5859bd182ee2ab2404aacadb
Mikhail Zhuravlev <m.zhuravlev61@gmail.com>
2016-07-16 22:54:01 +0700

    Updated login page.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,46 @@
-<form method="POST" action="/auth/login">
-    {!! csrf_field() !!}
-
-    <div>
-        Email
-        <input type="text" name="email" value="{{ old('email') }}">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Тестовый магазин</title>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/bootstrap-theme.min.css">
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+    <h1>Тестовый магазин</h1>
+    <div class="well">
+        @if (count($errors))
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{ print_r($error, true) }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <form method="POST" action="/auth/login">
+            {!! csrf_field() !!}
+            <div class="form-group">
+                <label for="login">Логин</label>
+                <input id="login" class="form-control" type="text" name="email" value="{{ old('email') }}">
+            </div>
+            <div class="form-group">
+                <label for="password">Пароль</label>
+                <input id="password" class="form-control" type="password" name="password">
+            </div>
+            <div class="form-group">
+                <label>
+                    <input type="checkbox" name="remember"> Запомнить меня
+                </label>
+            </div>
+            <div class="form-group">
+                <button class="btn btn-default" type="submit">Войти</button>
+            </div>
+        </form>
     </div>
-
-    <div>
-        Пароль
-        <input type="password" name="password" id="password">
-    </div>
-
-    <div>
-        <input type="checkbox" name="remember"> Запомнить меня
-    </div>
-
-    <div>
-        <button type="submit">Войти</button>
-    </div>
-</form>
+</div>
+</body>
+</html>
